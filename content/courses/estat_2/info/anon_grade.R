ce61fad2064893b81c9c06723f482634d23d630f
rbstern <rbstern@gmail.com>
2022-11-16 23:23:39 -0300

    estat_2: aval 2

--- content/courses/estat_2/info/anon_grade.R
+++ content/courses/estat_2/info/anon_grade.R
@@ -1,12 +1,12 @@
 library(tidyverse)
 
-grades = "./content/courses/estat_2/grades/estat2.csv" %>% 
+grades = "./content/courses/estat_2/info/estat2.csv" %>% 
   read_csv() %>%
-  select(NUSP, P1) %>% 
+  select(NUSP, P1, P2) %>% 
   mutate(
     NUSP = gsub("(P) ", "", NUSP, fixed = TRUE),
     NUSP = as.numeric(NUSP),
     NUSP = NUSP %% 1000
   )
 
-write_csv(grades, "./content/courses/estat_2/grades/grades.csv")
+write_csv(grades, "./content/courses/estat_2/info/grades.csv")
