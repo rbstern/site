d0c8df64325bc064eca4efbb38c54c8028312c64
rbstern <rbstern@gmail.com>
2022-12-22 10:32:50 -0300

    :estat_2: notas finais

--- content/courses/estat_2/info/anon_grade.R
+++ content/courses/estat_2/info/anon_grade.R
@@ -2,7 +2,7 @@ library(tidyverse)
 
 grades = "./content/courses/estat_2/info/estat_2.csv" %>% 
   read_csv() %>%
-  select(NUSP, P1, P2, P3, Part) %>% 
+  select(NUSP, P1, P2, P3, Part, Sub) %>% 
   mutate(
     NUSP = gsub("(P) ", "", NUSP, fixed = TRUE),
     NUSP = as.numeric(NUSP),
